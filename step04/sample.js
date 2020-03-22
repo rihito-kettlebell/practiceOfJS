27472949adbc002d6f670f8ed2329e46ce08fe83
m.t-protain <m.t1portfo@gmail.com>
2020-03-23 00:34:20 +0900

    変数の利用とイベント

---
+++ step04/sample.js
@@ -0,0 +1,17 @@
+// display resut of culculation
+function kotae() {
+  let kai = 37;
+  alert(kai);
+}
+
+// display resut of culculation
+function kotae2() {
+  let yama = "富士山";
+  alert(yama);
+}
+
+// display resut of culculation
+function kotae3() {
+  let kinniku = "大胸筋";
+  alert(kinniku);
+}
