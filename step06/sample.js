70760222aed7188d7f93c4c79a597835a881f48a
m.t-protain <m.t1portfo@gmail.com>
2020-03-24 00:17:47 +0900

    配列の学習

---
+++ step06/sample.js
@@ -0,0 +1,33 @@
+// define global variable
+let kai;
+let a = new Array(3);
+let b = new Array(3);
+
+a[0] = 5;
+a[1] = 12;
+a[2] = 18;
+b[0] = 33;
+b[1] = 14;
+b[2] = 65;
+
+
+// 積
+function seki0(){
+  kai = a[0] * b[0];
+  console.log(kai);
+  alert("答えは"+kai+"です");
+}
+
+// 積
+function seki1(){
+  kai = a[1] * b[1];
+  console.log(kai);
+  alert("答えは"+kai+"です");
+}
+
+// 積
+function seki2(){
+  kai = a[2] * b[2];
+  console.log(kai);
+  alert("答えは"+kai+"です");
+}
