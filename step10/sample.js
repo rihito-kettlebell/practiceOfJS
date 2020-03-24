33319fb3bf47f432200a14a316a55c9f490dd44a
m.t-protain <m.t1portfo@gmail.com>
2020-03-25 01:28:59 +0900

    条件分岐の学習

---
+++ step10/sample.js
@@ -0,0 +1,7 @@
+function judge(x) {
+  if(x == 4){
+    alert("正解だよ！")
+  }else{
+    alert("おしい、不正解だよ！")
+  }
+}
---
+++ step10/sample2.js
@@ -0,0 +1,9 @@
+let count = 0;
+function judge() {
+  count++;
+  if(count == 1){
+    alert("[ヒント]東北地方にある県です（今回は北海道はのぞきます)");
+  }else{
+    alert("答えは岩手県です");
+  }
+}
