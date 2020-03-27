1e6f579fd6c347e6cb6a1637117954490059cc17
m.t-protain <rihitosakuraa155@gmail.com>
2020-03-28 00:45:31 +0900

    プロンプトを利用し、入力を受け付ける

---
+++ step14/sample.js
@@ -0,0 +1,16 @@
+function koyakusu() {
+  let a, b, small, ax, bx;
+  a = prompt("1つ目の数値を入力してください");
+  b = prompt("2つ目の数値を入力してください");
+  if (a >= b) {
+    small = b;
+  } else {
+    small = a;
+  }
+  for (i = small; i >= 1; i--) {
+    if (a % i == 0 && b % i == 0) {
+      alert("最大公約数は" + i + "です");
+      break;
+    }
+  }
+}
