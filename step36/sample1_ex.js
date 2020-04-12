4487e4c98396f4a8051a3f79867216822b669284
m.t-protain <rihitosakuraa155@gmail.com>
2020-04-12 12:51:31 +0900

    イベント配下のthis

---
+++ step36/sample1_ex.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(e){
+  let data = {
+    title: "java",
+    price: 2680,
+    show: function(){
+      console.log(this.title + "/" + this.price + "円");
+    } 
+  };
+
+  // 以下ではエラーになる
+  // document.getElementById("btn").addEventListener("click", data.show, false);
+  // 以下で書き換える
+  document.getElementById("btn").addEventListener("click", data.show.bind(data), false);
+}, false);
